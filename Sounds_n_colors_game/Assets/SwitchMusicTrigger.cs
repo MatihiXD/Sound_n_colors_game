2883bf9b64d906880c16e1b558cc183c181f3d44
Mathieu <mathieu.brandao@epitech.eu>
2021-06-20 13:52:17 +0200

    [MOD] optimising some script

--- Sounds_n_colors_game/Assets/SwitchMusicTrigger.cs
+++ Sounds_n_colors_game/Assets/SwitchMusicTrigger.cs
@@ -5,18 +5,11 @@ using UnityEngine;
 public class SwitchMusicTrigger : MonoBehaviour
 {
     public AudioClip newTrack;
-
     private AudioManager theAM;
-    // Start is called before the first frame update
+
     void Start()
     {
         theAM = FindObjectOfType<AudioManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void OnTriggerEnter2D(Collider2D other)
