b6abef96ecc8e0211675496a46b34e8bacfe9986
Kira-Yam <loic.ding@epitech.eu>
2021-06-20 21:15:19 +0200

    [feat] mouse and spider hit sound

--- Sounds_n_colors_game/Assets/weakspots/mouse_weakness.cs
+++ Sounds_n_colors_game/Assets/weakspots/mouse_weakness.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class mouse_weakness : MonoBehaviour
 {
     public GameObject objectToDestroy;
+    public AudioSource source;
+    
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            if (KeyItems.instance.flute != 0)
+            if (KeyItems.instance.flute != 0) {
+                source.Play();
                 Destroy(objectToDestroy);
+            }
         }
     }
 }
--- Sounds_n_colors_game/Assets/weakspots/spider_weakness.cs
+++ Sounds_n_colors_game/Assets/weakspots/spider_weakness.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class spider_weakness : MonoBehaviour
 {
     public GameObject objectToDestroy;
+    public AudioSource source;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            if (KeyItems.instance.guitar != 0)
+            if (KeyItems.instance.guitar != 0) {
+                source.Play();
                 Destroy(objectToDestroy);
+            }
         }
     }
 }
